006f54b4af226336a549f5e24b78bf6ef391e887
JunYeong0314 <96372487+JunYeong0314@users.noreply.github.com>
2024-10-09 14:23:50 +0900

    Response모듈 추가

--- src/controller/common/baseResponse.js
+++ src/controller/common/baseResponse.js
@@ -1,3 +1,5 @@
+import { Response } from 'express';
+
 export const baseResponse = (res, success, message, data = null) => {
   return res.status(success ? 200 : 400).json({
     status: success ? 200 : 400,
