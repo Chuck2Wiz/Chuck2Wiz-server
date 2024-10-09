d7026a414eb0649b65a8e26c00357457f10a7754
JunYeong0314 <96372487+JunYeong0314@users.noreply.github.com>
2024-10-09 14:41:42 +0900

    응답수정

--- src/controller/common/baseResponse.js
+++ src/controller/common/baseResponse.js
@@ -1,8 +1,5 @@
-import { Response } from 'express';
-
 export const baseResponse = (res, success, message, data = null) => {
   return res.status(success ? 200 : 400).json({
-    status: success ? 200 : 400,
     success: success,
     message: message,
     data: data !== null ? data : {},
