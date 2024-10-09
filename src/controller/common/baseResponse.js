7f4540b0cd7206e322240cbc5171563fb5e144ad
JunYeong0314 <96372487+JunYeong0314@users.noreply.github.com>
2024-10-09 13:14:44 +0900

    로그추가

--- src/controller/common/baseResponse.js
+++ src/controller/common/baseResponse.js
@@ -1,3 +1,4 @@
 export const baseResponse = (res, success, message, data = null) => {
+  console.log('baseResponse called:', { success, message, data });
   return res.status(success ? 200 : 400).json({ success, message, data });
 };
